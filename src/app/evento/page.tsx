2a5bf83ab330bfb7d81151fe7fa53cd83dc6878b
Carlos Barros <55989012+barros-carlos@users.noreply.github.com>
2024-11-29 17:58:43 -0300

    Revert "Tela Evento - Início do Desenvolvimento"
    This reverts commit a213cd31734b9254796bf710a4bac1f8693ea72c.

---
+++ src/app/evento/page.tsx
@@ -0,0 +1,9 @@
+// essa é a visualização da página de um evento específico, como "Desafio da ODS", ou "Conheça a DevCommunity"
+
+import Navbar from "../components/Navbar";
+import "./style.css";
+export default function Evento() {
+    return (
+        <Navbar text="Evento" anchor="/home"></Navbar>
+      );
+} 
