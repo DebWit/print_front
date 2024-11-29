40732f46ba4333ac33f16b00b20f830ed242aa90
Carlos Barros <55989012+barros-carlos@users.noreply.github.com>
2024-11-29 02:12:55 -0300

    fix: anchor names fixeds

--- src/app/components/BottomBar.tsx
+++ src/app/components/BottomBar.tsx
@@ -5,8 +5,8 @@ export default function BottomBar({ disabled = undefined }: { disabled?: number 
     return (
         <>
             <div className="fixed bottom-0 flex flex-wrap md:justify-content-center justify-content-evenly align-items-center w-full mx-0 h-4rem mt-4" style={{backgroundColor: "#060153"}}>
-                <a href={disabled === 1 ? "#" : "/maps"} className={className} style={{textDecoration: "none", width: "20%", background: (disabled === 1 ? "#2B22BB99" : "")  }}><i className="pi pi-map font-light" style={{ fontSize: '2rem' }}></i></a>
-                <a href={disabled === 2 ? "#" : "/my-calendar"} className={className} style={{textDecoration: "none", width: "20%", background: (disabled === 2 ? "#2B22BB99" : "") }}><i className="pi pi-calendar font-light" style={{ fontSize: '2rem' }}></i></a>
+                <a href={disabled === 1 ? "#" : "/mapa"} className={className} style={{textDecoration: "none", width: "20%", background: (disabled === 1 ? "#2B22BB99" : "")  }}><i className="pi pi-map font-light" style={{ fontSize: '2rem' }}></i></a>
+                <a href={disabled === 2 ? "#" : "/minhaagenda"} className={className} style={{textDecoration: "none", width: "20%", background: (disabled === 2 ? "#2B22BB99" : "") }}><i className="pi pi-calendar font-light" style={{ fontSize: '2rem' }}></i></a>
                 <a href={disabled === 3 ? "#" : "/home"} className={className} style={{textDecoration: "none", width: "20%", background: (disabled === 3 ? "#2B22BB99" : "") }}><i className="pi pi-home font-light" style={{ fontSize: '2rem'}}></i></a>
                 <a href={disabled === 4 ? "#" : "/eventos"} className={className} style={{textDecoration: "none", width: "20%", background: (disabled === 4 ? "#2B22BB99" : "") }}><i className="pi pi-bars font-light" style={{ fontSize: '2rem' }}></i></a>
                 <a href={disabled === 5 ? "#" : "/notificacoes"} className={className} style={{textDecoration: "none", width: "20%", background: (disabled === 5 ? "#2B22BB99" : "") }}><i className="pi pi-bell font-light" style={{ fontSize: '2rem' }}></i></a>
