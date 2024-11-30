22d59d1c072068061f4717a5e6186b3c67e3e0db
Carlos Barros <55989012+barros-carlos@users.noreply.github.com>
2024-11-30 12:59:30 -0300

    fix: removing awful dev indicator

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: {
+    appIsrStatus: false,
+  },
 };
 
 export default nextConfig;
